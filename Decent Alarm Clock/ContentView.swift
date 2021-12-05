4d15bb87247dfd7a653d596cae4f164e99ac7ef2
Ataberk <olgunataberk@gmail.com>
2021-12-05 20:21:00 +0300

    Initial Commit

---
+++ Decent Alarm Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Decent Alarm Clock
+//
+//  Created by Ataberk Olgun on 5.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Decent Alarm Clock/Decent_Alarm_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Decent_Alarm_ClockApp.swift
+//  Decent Alarm Clock
+//
+//  Created by Ataberk Olgun on 5.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Decent_Alarm_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
